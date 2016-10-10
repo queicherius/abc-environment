738512457eed825811ec9027a0c642ba93ef67b2
queicherius <queicherius@gmail.com>
2016-10-10 16:32:55 +0200

    Include subfolders when linting

--- src/lint.js
+++ src/lint.js
@@ -2,7 +2,7 @@ var log = require('./helpers/log.js')
 var exec = require('exec-sh')
 
 module.exports = function () {
-  var command = 'NODE_ENV=development $(npm bin)/snazzy {src,tests,.}/*.js --parser babel-eslint'
+  var command = 'NODE_ENV=development $(npm bin)/snazzy {src,tests,.}/**/*.js --parser babel-eslint'
 
   log.info('Running lint')
   exec(command, function (err) {
