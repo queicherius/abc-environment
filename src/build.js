cae330e6b4cdfe551cb59e56466b19c4ffe3f6fb
queicherius <queicherius@gmail.com>
2016-10-22 22:45:21 +0200

    Make sure the build directory is clean

--- src/build.js
+++ src/build.js
@@ -2,7 +2,7 @@ var log = require('./helpers/log.js')
 var exec = require('exec-sh')
 
 module.exports = function () {
-  var command = 'NODE_ENV=production $(npm bin)/babel src/ --out-dir build/'
+  var command = 'rm -rf build/ && NODE_ENV=production $(npm bin)/babel src/ --out-dir build/'
 
   log.info('Building files from `src/` into `build/`')
   exec(command, function (err) {
