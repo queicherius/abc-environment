2af8c1d2919d0cbe9dd92c28058d0bd199d08836
queicherius <queicherius@gmail.com>
2017-01-11 02:59:58 +0100

    Remove whitespace & use same order as in readme

--- src/index.js
+++ src/index.js
@@ -4,12 +4,14 @@ var log = require('./helpers/log.js')
 var whitelist = ['build', 'run', 'test', 'lint', 'setup']
 
 if (argv.length === 0 || argv[0] === 'help') {
-  console.log('abc [command]\n\nCommands:')
-  console.log('  build            Compiles the source files from src/ into a build/ directory. ')
-  console.log('  run src/file.js  Runs a specified file.')
-  console.log('  test             Runs the test files in the tests/ directory.')
-  console.log('  lint             Checks if your code is consistent and follows best practices. ')
+  console.log('abc [command]')
+  console.log('')
+  console.log('Commands:')
   console.log('  setup            Creates the directories and files for the whole project to run.')
+  console.log('  build            Compiles the source files from src/ into a build/ directory.')
+  console.log('  test             Runs the test files in the tests/ directory.')
+  console.log('  lint             Checks if your code is consistent and follows best practices.')
+  console.log('  run src/file.js  Runs a specified file.')
   console.log('  help             This help page you are looking at!')
   process.exit(0)
 }
